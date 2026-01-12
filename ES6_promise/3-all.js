6b885eaa873ddae60e15a379a4c381047013829f
zoulouhh <azladji.sil@gmail.com>
2026-01-12 09:42:56 +0000

    all

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const [photo, user] = values;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
