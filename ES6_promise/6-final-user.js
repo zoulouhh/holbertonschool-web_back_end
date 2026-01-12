e9bb03eb862e5f8d9d82bcf4041dd0217e499ab1
zoulouhh <azladji.sil@gmail.com>
2026-01-12 09:47:47 +0000

    all

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -8,7 +8,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   ]).then((results) =>
     results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      value:
+        result.status === 'fulfilled'
+          ? result.value
+          : result.reason.toString(),
     }))
   );
 }
