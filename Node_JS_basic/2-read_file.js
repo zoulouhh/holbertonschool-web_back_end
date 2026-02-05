f5bb9fc113f88d18ccf1b5f8a123b7abe1f2c7c7
zoulouhh <azladji.sil@gmail.com>
2026-02-05 14:35:11 +0000

    all

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -8,29 +8,33 @@ function countStudents(path) {
     throw new Error('Cannot load the database');
   }
 
-  const lines = data.split('\n');
-  const students = lines.filter((line) => line && line !== lines[0]);
+  const lines = data
+    .split('\n')
+    .filter((line) => line.trim() !== '');
+
+  const students = lines.slice(1);
+
   console.log(`Number of students: ${students.length}`);
 
   const fields = {};
-  const columns = lines[0].split(',');
-  const firstNamePos = columns.indexOf('firstname');
-  const fieldPos = columns.indexOf('field');
 
-  for (const student of students) {
-    const studentData = student.split(',');
-    const field = studentData[fieldPos];
-    const firstName = studentData[firstNamePos];
+  students.forEach((line) => {
+    const parts = line.split(',');
+    const firstName = parts[0];
+    const field = parts[3];
 
-    if (!fields[field]) fields[field] = [];
-    fields[field].push(firstName);
-  }
-
-  for (const field in fields) {
-    if (Object.prototype.hasOwnProperty.call(fields, field)) {
-      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    if (!fields[field]) {
+      fields[field] = [];
     }
-  }
+    fields[field].push(firstName);
+  });
+
+  Object.keys(fields).forEach((field) => {
+    const list = fields[field].join(', ');
+    console.log(
+      `Number of students in ${field}: ${fields[field].length}. List: ${list}`,
+    );
+  });
 }
 
 module.exports = countStudents;
