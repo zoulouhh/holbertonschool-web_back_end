b6c2ef9a3a9f2bf64141214200857214234161fa
zoulouhh <azladji.sil@gmail.com>
2026-02-05 14:33:10 +0000

    all

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,10 +3,10 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  const name = data.toString().replace(/\n$/, '');
+  process.stdout.write(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write('\nThis important software is now closing\n');
 });
